a6ddf4a32722f7970226a63f99837dd93b976f96
YMK <ymk@inbox.ru>
2019-05-27 02:55:13 +0300

    render for orders in personal room

--- public/lk.php
+++ public/lk.php
@@ -8,6 +8,9 @@ if(isset($_SESSION['user'])) {
 	$Content .= 'Ваш EMail: ' . $_SESSION['user']['email'] . '<br>';
 	$Content .= 'Ваш ID: ' . $_SESSION['user']['id'] . '<br>';
 	$Content .= '<a href="/logout.php">Выйти</a><br>';
+
+	$orders = getOrder($_SESSION['user']['id']);
+	$Content .= renderOrders($orders);
 }
 
 echo render(TEMPLATES_DIR . 'index.tpl', [
@@ -15,3 +18,19 @@ echo render(TEMPLATES_DIR . 'index.tpl', [
 	'h1' => 'Личный кабинет',
 	'content' => $Content
 ]);
+
+function getOrder($id)
+{
+	$id = (int)$id;
+	$sql = "SELECT * FROM orders WHERE `userId` = '$id' ORDER BY `orders`.`id` DESC";
+	return getAssocResult($sql);
+}
+
+function renderOrders($orders)
+{
+	$content = '';
+	foreach ($orders as $orderItem) {
+		$content .= render(TEMPLATES_DIR . 'orderItem.tpl', $orderItem);
+	}
+	return $content;
+}
---
+++ templates/orderItem.tpl
@@ -0,0 +1,4 @@
+<div class="order-item">
+	<h3>Заказ № {{ID}}</h3>
+	<a href="updateOrder.php?orderid={{ID}}" target="_blank">{{DATECREATE}} , {{STATUS}}</a>
+</div>
