c6a42bd323a88062039cc249fa2a4d2256482131
YMK <ymk@inbox.ru>
2019-05-26 22:03:20 +0300

    $_SESSION changes from items to array

--- public/logout.php
+++ public/logout.php
@@ -2,17 +2,14 @@
 
 require_once __DIR__ . '/../config/config.php';
 
-$Content = "";
-if(isset($_SESSION['login'])) {
-	$Content = 'Всего доброго, ' . $_SESSION['login'] . '<hr>';
-	unset($_SESSION['login']);
-}
-if(isset($_SESSION['id'])) {
-	unset($_SESSION['id']);
+$content = "";
+if(isset($_SESSION['user'])) {
+	$content = 'Всего доброго, ' . $_SESSION['user']['login'] . '<hr>';
+	unset($_SESSION['user']);
 }
 
 echo render(TEMPLATES_DIR . 'index.tpl', [
 	'title' => 'Магазин',
 	'h1' => 'Вы завершили сессию',
-	'content' => $Content
+	'content' => $content
 ]);
