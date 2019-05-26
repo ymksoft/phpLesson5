89586a2f27de6a564c22403231404e574d2752f2
YMK <ymk@inbox.ru>
2019-05-26 22:01:32 +0300

    add (int)$id

--- public/newsItem.php
+++ public/newsItem.php
@@ -2,9 +2,7 @@
 
 require_once __DIR__ . '/../config/config.php';
 
-//var_dump($_GET['id']);
-
-$id = isset($_GET['id']) ? $_GET['id'] : false;
+$id = isset($_GET['id']) ? (int)$_GET['id'] : false;
 
 if(!$id) {
 	echo 'id не передан';
