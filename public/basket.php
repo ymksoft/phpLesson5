1519a52b8de3fade50212bd85012bcacbd6d3b34
YMK <ymk@inbox.ru>
2019-05-27 02:54:22 +0300

    +add output basket

--- public/basket.php
+++ public/basket.php
@@ -2,15 +2,49 @@
 
 require_once __DIR__ . '/../config/config.php';
 
-$Content = "";
-if(isset($_SESSION['login'])) {
-	$Content .= 'Здраствуйте, ' . $_SESSION['login'] . '<br>';
-	$Content .= 'Ваш EMail: ' . $_SESSION['email'] . '<br>';
-	$Content .= 'Ваш ID: ' . $_SESSION['id'] . '<br>';
-}
+$content = renderBacket($_COOKIE['Basket']);
 
 echo render(TEMPLATES_DIR . 'index.tpl', [
-	'title' => 'Магазин',
-	'h1' => 'Личный кабинет',
-	'content' => $Content
+	'title' => 'Корзина',
+	'h1' => 'Список товаров в корзине',
+	'content' => $content
 ]);
+
+
+function renderBacket($basket) {
+	$content = "";
+
+	if( empty($basket) ) {
+		$content .= "Ваша корзина пока пуста!<br>";
+		return $content;
+	}
+
+	$ids = array_keys($basket);
+	$goods = getGoodsByIds($ids);
+
+	$totalAmount = 0;
+	$totalPrice = 0;
+	foreach($goods as $good)	 {
+		$amount = $basket[$good['id']];
+		$price = $good['price'];
+		$sum = $price * $amount;
+		$content .= render(TEMPLATES_DIR . 'basketListItem.tpl', [
+			'name' => $good['name'],
+			'id' => $good['id'],
+			'amount' => strval( $amount ),
+			'price' => strval( $good['price'] ),
+			'sum' => strval( $sum )
+		]);
+		$totalAmount += $amount;
+		$totalPrice += $sum;
+	}
+
+	return render(TEMPLATES_DIR . 'basketList.tpl', [
+		'amount' => strval( $totalAmount ),
+		'sum' => strval ($totalPrice ),
+		'content' => $content,
+		'order' => empty($_SESSION['user']) 
+			? '<a href="/login.php">Войти</a>'
+			: '<a href="/createOrder.php">Оформить заказ</a>'
+	]);;
+}
